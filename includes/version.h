72038cee730a096b5b44b873fcc043a0fc84e1f6
Matt Ryder <matt@mattryder.co.uk>
2011-10-22 03:42:03 +0100

    Cleaned up. Version is now 1.0.0.1

--- includes/version.h
+++ includes/version.h
@@ -2,8 +2,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
+///Returns the Operating System's Version.
+
 char* kVersion() {
-  return "1.0.0.0";
+  return "1.0.0.1";
 }
 
 #endif 
--- kernel.c
+++ kernel.c
@@ -2,27 +2,21 @@
 #include "includes/version.h"
 #include "kernel.h"
 #include "includes/string.h"
+#include "includes/debug.h"
 
 /* xmain:
  * MAIN ENTRY POINT FROM THE BOOTLOADER
  */
 void xmain( void* mbd, unsigned int magic )
 {
+  
   if( magic != 0x2BADB002 )
   {
-    //Output an error!
+    //Output an error! [THIS IS NEVER HIT! USE ASM TO DO THIS IN LOADER.S!]
     kprintf("Something went wrong! Didn't get GRUB MAGIC [0x2BADB002]");
   }
   print_welcome_message(); 
-}
-
-/* dump_registers:
- * Writes the contents of the general registers to the
- * terminal in a human-readable format
- */
-void dump_registers()
-{
-
+  
 }
 
 /* print_welcome_message:
