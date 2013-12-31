b43109874b985e1474fd4b3cd45b83b57fb0517c
Matt Ryder <matt@mattryder.co.uk>
2013-12-31 21:02:33 +0000

    Fix up the makefile and build script

--- Makefile
+++ Makefile
@@ -1,61 +1,38 @@
-CFLAGS= -Wall -Wextra -nostdlib -nostartfiles -nodefaultlibs
+CFLAGS= -Wall -Wextra -nostdlib -nostartfiles -nodefaultlibs -c -m32
+AFLAGS= -f elf
+LDFLAGS= -m elf_i386 -s -T linker.ld
+
+CC = cc
+ASSEMBLER = nasm
+LD = ld
 
 #Build Directory for .o and .bin
-BUILD_DIR = build
+BUILD_DIR = build/
 
-#Build Directory for core objects
-BUILD_COMMON = $(BUILD_DIR)/common
-BUILD_INTERRUPTS = $(BUILD_DIR)/interrupts
+# Collect all the src files from the directories
+SRC_KERNEL = $(wildcard *.c)
+SRC_ASM = $(wildcard ASM/*.s)
+SRC_COMMON = $(wildcard common/*.c)
+SRC_INTERRUPTS = $(wildcard interrupts/*.c)
+
+ALL_SRC = $(SRC_KERNEL) $(SRC_ASM) $(SRC_COMMON) $(SRC_INTERRUPTS)
+BUILD_TARGETS = $(patsubst %.s, %.o, $(patsubst %.c, %.o, $(ALL_SRC)))
+
+OBJECTS = $(notdir $(patsubst %.s, %.o, $(patsubst %.c, %.o, $(ALL_SRC))))
+BUILD_OBJECTS = $(foreach OBJECT, $(OBJECTS), $(BUILD_DIR)$(OBJECT))
+
+ALL_OBJ = $(wildcard $(BUILD_DIR)*.o)
+
 K_OUTPUT = kernel.bin
 
-ALL_OBJECTS = kernel.o loader.o  \
-	      common.o monitor.o string.o \
-	      descriptor_tables.o isr.o interrupt.o flush_tables.o
+all: $(BUILD_TARGETS)
+	$(LD) $(LDFLAGS) -o $(BUILD_DIR)scratch/$(K_OUTPUT) $(BUILD_OBJECTS)
 
-STD_OBJECTS = $(BUILD_DIR)/kernel.o $(BUILD_DIR)/loader.o
-INTERRUPT_OBJECTS = $(BUILD_INTERRUPTS)/descriptor_tables.o $(BUILD_INTERRUPTS)/isr.o $(BUILD_INTERRUPTS)/interrupt.o $(BUILD_INTERRUPTS)/flush_tables.o
-COMMON_OBJECTS = $(BUILD_COMMON)/common.o  $(BUILD_COMMON)/monitor.o $(BUILD_COMMON)/string.o
+%.o: %.s
+	$(ASSEMBLER) $(AFLAGS) -o $(BUILD_DIR)$(@F) $^
 
-all: $(ALL_OBJECTS)
-	$(LD) -T linker.ld -o $(K_OUTPUT) $(STD_OBJECTS) $(COMMON_OBJECTS) $(INTERRUPT_OBJECTS)
-	mv $(K_OUTPUT) $(BUILD_DIR)/scratch/$(K_OUTPUT)
-
-kernel.o:
-	cc $(CFLAGS) -c kernel.c
-	mv kernel.o $(BUILD_DIR)/kernel.o
-
-loader.o:
-	nasm -f elf -o loader.o ASM/loader.s
-	mv loader.o $(BUILD_DIR)/loader.o
-
-common.o:
-	cc -c $(CFLAGS) common/common.c
-	mv common.o $(BUILD_COMMON)/common.o
-
-monitor.o:
-	cc -c $(CFLAGS) common/monitor.c
-	mv monitor.o $(BUILD_COMMON)/monitor.o
-
-string.o:
-	cc -c $(CFLAGS) common/string.c
-	mv string.o $(BUILD_COMMON)/string.o
-
-descriptor_tables.o:
-	cc -c $(CFLAGS) interrupts/descriptor_tables.c
-	mv descriptor_tables.o $(BUILD_INTERRUPTS)/descriptor_tables.o
-
-isr.o:
-	cc -c $(CFLAGS) interrupts/isr.c
-	mv isr.o $(BUILD_INTERRUPTS)/isr.o
-
-interrupt.o:
-	nasm -f elf -o interrupt.o ASM/interrupt.s
-	mv interrupt.o $(BUILD_INTERRUPTS)/interrupt.o
-
-flush_tables.o:
-	nasm -f elf -o flush_tables.o ASM/flush_tables.s
-	mv flush_tables.o $(BUILD_INTERRUPTS)/flush_tables.o
+%.o: %.c
+	$(CC) $(CFLAGS) -o $(BUILD_DIR)$(@F) $^
 
 clean:
-	rm -rf --verbose $(STD_OBJECTS) $(COMMON_OBJECTS) $(INTERRUPT_OBJECTS) \
-    $(BUILD_DIR)/$(K_OUTPUT) $(BUILD_DIR)/scratch/$(K_OUTPUT)
+	rm -rf --verbose $(ALL_OBJ) mattos.img
--- kbuild.sh
+++ kbuild.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # MATT OS Boot Script - boot MattOS from CLI.
 
+# Used to boot whatever emulator we're using with special rules for OSX.
+function launch() {
+  #Launch emulator, depending on OS:
+
+  if [ ${UNAME} = "Darwin" ]; then
+    emulator="/Applications/Q.app/Contents/MacOS/i386-softmmu.app/Contents/MacOS/i386-softmmu" #Q.App's i386 emulator
+    ${emulator} ${built_image}
+
+  elif [ ${UNAME} = "Linux" ]; then
+    #boot bochs in a new tab, keep the clutter in it's own tab:
+    $SHELL -c "qemu-system-i386 -kernel ${scratch_dir}${kernel_binary}"
+  fi
+}
+
 # Grab the host platform:
 UNAME=$(uname)
 
@@ -14,19 +28,39 @@ if [ ${UNAME} = "Darwin" ]; then
 fi
 
 # CONFIG PARAMETERS
-scratch_dir="build/scratch/"
+build_dir="build/"
+scratch_dir=${build_dir}"scratch/"
 harddisk_image="mattos.img"
 kernel_binary="kernel.bin"
-built_image="build/"${harddisk_image}
+built_image=${build_dir}${harddisk_image}
 cat_args=${scratch_dir}"stage1 "${scratch_dir}"stage2 "${scratch_dir}"pad "${scratch_dir}${kernel_binary}
 
 clear
 
-#make sure padding and stage 1 + 2 exist:
-checkRequiredFiles
+# create pad and check that stage1+2 exist:
+function checkPrerequisiteFiles() {
+  req_files=("stage1" "stage2" "kernel.bin")
+
+  if [ ! -f ${scratch_dir}"pad" ];
+  then
+    echo "[WARNING] pad file doesn't exist! Building 750b pad:\n";
+    dd if=/dev/zero of=pad bs=1 count=750;
+    echo "pad built!\n";
+  fi
+
+  for req_file in "${req_files[@]}"; do
+    if [ ! -f ${scratch_dir}${req_file} ]; then
+      echo "[ERROR] Terminating because "${req_file}" doesn't exist!"
+      exit 1
+    fi
+  done
+}
 
 make clean
 make all
+
+#make sure padding, stage1/2 and kernel.bin exist:
+checkPrerequisiteFiles
 
 #compile into floppy image from kernel:
 cat $cat_args > ${built_image}
@@ -37,51 +71,12 @@ then
   exit 2
 fi
 
-#Because i'm lazy, make bash do the kernel boot sector math :P
+# This isn't really needed, but it's nice to know
 KSIZE=$(stat -c%s "$built_image")
-BOOTSECTOR=$(expr ${KSIZE} / 512)
-echo -e "Kernel Size: "${KSIZE}" bytes. Kernel Boot Sector: "${BOOTSECTOR}
+BSECT=$(expr ${KSIZE} / 512) 
+echo -e "Kernel Size: "${KSIZE}" bytes. Kernel Boot Sector: "${BSECT} 
 
 echo -e 'Build Successful! Booting Matt OS:'
-
-function checkRequiredFiles() {
-  # create pad and check that stage1+2 exist:
-  if [ ! -f ${scratch_dir}"pad" ];
-  then
-    echo "[WARNING] pad file doesn't exist! Building 750b pad:\n";
-    dd if=/dev/zero of=pad bs=1 count=750;
-    echo "pad built!\n";
-  fi
-
-  if [ ! -f ${scratch_dir}"stage1" ];
-  then
-    echo "[ERROR] stage1 doesn't exist! Terminating!\n";
-    exit 1;
-  fi
-
-  if [ ! -f ${scratch_dir}"stage2" ];
-  then
-    echo "[ERROR] stage2 doesn't exist! Terminating!\n";
-    exit 1;
-  fi
-
-  if [ ! -f ${scratch_dir}${kernel_binary} ];
-  then
-    echo "[ERROR] failed to build kernel.bin! Terminating!";
-    exit 2;
-  fi
-}
-
-function launch() {
-  #Launch emulator, depending on OS:
-  if [ ${UNAME} = "Darwin" ]; then
-    emulator="/Applications/Q.app/Contents/MacOS/i386-softmmu.app/Contents/MacOS/i386-softmmu" #Q.App's i386 emulator
-    ${emulator} ${built_image}
-
-  elif [ ${UNAME} = "Linux" ]; then
-    #boot bochs in a new tab, keep the clutter in it's own tab:
-    gnome-terminal -e "bash -c \"exec "${bochs}"\""
-  fi
-}
+launch
 
 
